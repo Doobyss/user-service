ae1d039b1a99fe4034ce08f6a747f37170f49e22
Doobyss <abzalish2@yandex.ru>
2025-12-05 22:27:39 +0500

    Add files via upload
    шестая домашка

--- user-service/src/main/java/com/aston/userservice/controller/Controller.java
+++ user-service/src/main/java/com/aston/userservice/controller/Controller.java
@@ -2,8 +2,13 @@ package com.aston.userservice.controller;
 
 import com.aston.userservice.dto.UserRequestDto;
 import com.aston.userservice.dto.UserResponseDto;
+import com.aston.userservice.hateoas.UserModel;
+import com.aston.userservice.hateoas.UserModelAssembler;
 import com.aston.userservice.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,30 +19,40 @@ import java.util.List;
 public class Controller {
 
     private final UserService service;
+    private final UserModelAssembler assembler;
 
+    @Operation(summary = "Создать пользователя")
     @PostMapping
-    public UserResponseDto create(@RequestBody UserRequestDto dto) {
-        return service.create(dto);
+    public UserModel create(@RequestBody UserRequestDto dto) {
+        UserResponseDto user = service.create(dto);
+        return assembler.toModel(user);
     }
 
+    @Operation(summary = "Получить пользователя по ID")
     @GetMapping("/{id}")
-    public UserResponseDto get(@PathVariable Long id) {
-        return service.getById(id);
+    public UserModel get(@PathVariable Long id) {
+        UserResponseDto user = service.getById(id);
+        return assembler.toModel(user);
     }
 
+    @Operation(summary = "Получить список всех пользователей")
     @GetMapping
-    public List<UserResponseDto> getAll() {
-        return service.getAll();
+    public CollectionModel<UserModel> getAll() {
+        List<UserResponseDto> users = service.getAll();
+        return assembler.toCollectionModel(users);
     }
 
+    @Operation(summary = "Обновить пользователя")
     @PutMapping("/{id}")
-    public UserResponseDto update(@PathVariable Long id,
-                                  @RequestBody UserRequestDto dto) {
-        return service.update(id, dto);
+    public UserModel update(@PathVariable Long id, @RequestBody UserRequestDto dto) {
+        UserResponseDto user = service.update(id, dto);
+        return assembler.toModel(user);
     }
 
+    @Operation(summary = "Удалить пользователя")
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id) {
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
         service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
