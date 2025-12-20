d33f785c87e2429d439fbe07724f0d368db6fb5e
Doobyss <abzalish2@yandex.ru>
2025-12-20 18:06:22 +0500

    Add files via upload
    Восьмая домашка, подготовка сервиса к сборке

--- user-service/src/test/java/com/aston/userservice/controller/UserControllerTest.java
+++ user-service/src/test/java/com/aston/userservice/controller/UserControllerTest.java
@@ -1,6 +1,9 @@
 package com.aston.userservice.controller;
 
 import com.aston.userservice.dto.UserRequestDto;
+import com.aston.userservice.dto.UserResponseDto;
+import com.aston.userservice.hateoas.UserModel;
+import com.aston.userservice.hateoas.UserModelAssembler;
 import com.aston.userservice.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -13,7 +16,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(Controller.class)
+@WebMvcTest(
+        controllers = Controller.class,
+        properties = "spring.config.import="
+)
 class UserControllerTest {
 
     @Autowired
@@ -22,20 +28,32 @@ class UserControllerTest {
     @MockBean
     private UserService service;
 
+    @MockBean
+    private UserModelAssembler assembler;
+
     @Test
     void testCreateUser() throws Exception {
 
+        UserResponseDto responseDto =
+                new UserResponseDto(1L, "Alex", "alex@mail.com", 30);
+
+        UserModel userModel = new UserModel(responseDto);
+
         Mockito.when(service.create(Mockito.any()))
-                .thenReturn(new com.aston.userservice.dto.UserResponseDto(
-                        1L, "Alex", "alex@mail.com", 30
-                ));
+                .thenReturn(responseDto);
+
+        Mockito.when(assembler.toModel(Mockito.any()))
+                .thenReturn(userModel);
 
         mvc.perform(post("/api/users")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("""
-                        {"name":"Alex","email":"alex@mail.com","age":30}
-                        """))
+        {"name":"Alex","email":"alex@mail.com","age":30}
+        """))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").value(1));
+                .andExpect(jsonPath("$.user.id").value(1))
+                .andExpect(jsonPath("$.user.name").value("Alex"))
+                .andExpect(jsonPath("$.user.email").value("alex@mail.com"));
     }
 }
+
